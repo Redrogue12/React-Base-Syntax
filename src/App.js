ed03795cb4b2c0c8d808255745a947a70a94c894
Redrogue12 <exgc12@gmail.com>
2018-08-30 21:21:28 -0400

    implemented components inside App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Components
+import UserInput from './components/UserInput'
+import UserOutput from './components/UserOutput'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <UserInput />
+        <UserOutput />
+
         <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
           <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
           <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
           <li>Add a method to manipulate the state (=> an event-handler method)</li>
