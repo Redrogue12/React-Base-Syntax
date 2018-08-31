9e7e91ef4a458df3d4bdf3bb9b01b619cb83a7cd
Redrogue12 <exgc12@gmail.com>
2018-08-30 21:19:43 -0400

    moved folder to src

---
+++ src/components/UserInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const userInput = (props) => {
+  return (
+    <div className="userInput">
+      <input></input>
+    </div>
+  )
+}
+
+export default userInput;
---
+++ src/components/UserOutput.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const userOutput = (props) => {
+  return (
+    <div className="userOutput">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Suspendisse venenatis, lacus nec luctus porttitor, erat mi fringilla lacus,
+        vestibulum convallis diam purus vehicula urna. Fusce vulputate metus metus,
+        sit amet scelerisque augue iaculis eget. Aenean iaculis mollis pellentesque.
+        Aenean lobortis, sem non vehicula tincidunt, velit est pulvinar turpis, quis
+        fermentum purus leo eget mauris. Nunc sit amet rhoncus ipsum. Nullam viverra
+        lacus elit, ac feugiat elit condimentum sed. Duis pretium efficitur diam in facilisis.
+        Suspendisse fringilla ex at ante mattis, ac elementum diam viverra. In ut accumsan nunc,
+        ac eleifend nibh. Aenean lacinia ex lectus, et pretium diam scelerisque in. Sed at nisl tellus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis, lacus nec luctus porttitor, erat mi fringilla lacus, vestibulum convallis diam purus vehicula urna. Fusce vulputate metus metus, sit amet scelerisque augue iaculis eget. Aenean iaculis mollis pellentesque. Aenean lobortis, sem non vehicula tincidunt, velit est pulvinar turpis, quis fermentum purus leo eget mauris. Nunc sit amet rhoncus ipsum. Nullam viverra lacus elit, ac feugiat elit condimentum sed. Duis pretium efficitur diam in facilisis. Suspendisse fringilla ex at ante mattis, ac elementum diam viverra. In ut accumsan nunc, ac eleifend nibh. Aenean lacinia ex lectus, et pretium diam scelerisque in. Sed at nisl tellus.
+      </p>
+    </div>
+  )
+}
+
+export default userOutput;
